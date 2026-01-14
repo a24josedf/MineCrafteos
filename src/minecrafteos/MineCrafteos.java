4a96875c2c6b70b5f5b96bb7af31752104601f76
alumno <alumno@10.0.2.15>
2026-01-14 14:08:32 +0100

    First commit

---
+++ src/minecrafteos/MineCrafteos.java
@@ -0,0 +1,16 @@
+package minecrafteos;
+
+/**
+ *
+ * @author alumno
+ */
+public class MineCrafteos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
